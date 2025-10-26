f0c66328bc8f0dccc43084daabc1adccce2ee849
realdeal008 <adeshina6233@gmail.com>
2025-10-26 12:35:09 -0700

    Update app for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,16 @@ import Navbar from "../Component/Navbar"
 import Hero from "../Component/Sections/Hero";
 import Booking from "@/Component/Sections/Booking";
 import Footer from "@/Component/Sections/Footer";
+import Luxury3DBackground from "../Component/Luxury3DBackground";
 
 export default function Home() {
   return (
-    <div>
-       <Navbar/>
-       <Hero/>
-       <Gallery/>
-       <Booking/>
-       <Footer/>
-    </div>
+    <Luxury3DBackground>
+      <Navbar/>
+      <Hero/>
+      <Gallery/>
+      <Booking/>
+      <Footer/>
+    </Luxury3DBackground>
   );
 }
